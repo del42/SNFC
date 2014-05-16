87b33cbd20b5eaf05ed96dad65319b44f0444623
del42 <delger42@gmail.com>
2014-05-16 14:35:20 -0700

    Initial Commit

---
+++ SNFC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SNFC
+//
+//  Created by delgersuren bold on 5/16/14.
+//  Copyright (c) 2014 delgersuren bold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SNFC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SNFC
+//
+//  Created by delgersuren bold on 5/16/14.
+//  Copyright (c) 2014 delgersuren bold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
